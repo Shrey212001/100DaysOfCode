8e25401105369af76e15631451fa76a25f4f2114
Shrey Srivastava <72334234+Shrey212001@users.noreply.github.com>
2022-12-08 14:27:14 +0530

    Java Lambda Expressions

---
+++ Day67.java
@@ -0,0 +1,46 @@
+/*
+Topic:- Java Lambda Expressions
+
+Link:- https://www.hackerrank.com/challenges/java-lambda-expressions/problem?isFullScreen=true
+
+Problem:-
+In this HackerRank Java Lambda Expressions problem in the java programming language you need to Write the following methods that return a lambda expression performing a specified action:
+
+PerformOperation isOdd(): The lambda expression must return true if a number is odd or false if it is even.
+
+PerformOperation isPrime(): The lambda expression must return true if a number is prime or false if it is composite.
+
+PerformOperation isPalindrome(): The lambda expression must return true if a number is a palindrome or false if it is not.
+
+Input Format
+
+Input is handled for you by the locked stub code in your editor.
+
+Output Format
+
+The locked stub code in your editor will print  lines of output.
+
+Sample Input
+
+The first line contains an integer,  (the number of test cases).
+
+The  subsequent lines each describe a test case in the form of  space-separated integers:
+The first integer specifies the condition to check for ( for Odd/Even,  for Prime, or  for Palindrome). The second integer denotes the number to be checked.
+
+5
+1 4
+2 5
+3 898
+1 3
+2 12
+
+Sample Output
+
+EVEN
+PRIME
+PALINDROME
+ODD
+COMPOSITE
+
+Solution:-
+*/
