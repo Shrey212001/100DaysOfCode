d7a5737a172224766793cb0d3842f8dee332f190
Shrey Srivastava <72334234+Shrey212001@users.noreply.github.com>
2022-12-16 19:59:24 +0530

    Time Conversion

---
+++ Day75.java
@@ -0,0 +1,85 @@
+/*
+Topic:- Time Conversion
+
+Link:- https://www.hackerrank.com/challenges/time-conversion/problem?isFullScreen=true
+
+Problem:-
+Given a time in 12-hour AM/PM format, convert it to military (24-hour) time.
+
+Note: – 12:00:00AM on a 12-hour clock is 00:00:00 on a 24-hour clock.
+– 12:00:00PM on a 12-hour clock is 12:00:00 on a 24-hour clock.
+
+Example
+
+s = ’12:01:00PM’
+Return ’12:01:00′.
+s = ’12:01:00AM’
+Return ’00:01:00′.
+
+
+Function Description
+
+Complete the timeConversion function in the editor below. It should return a new string representing the input time in 24 hour format.
+
+timeConversion has the following parameter(s):
+
+string s: a time in 12 hour format
+Returns
+
+string: the time in 24 hour format
+
+
+Input Format
+A single string s that represents a time in 12-hour clock format (i.e.: hh:mm:ssAM or hh:mm:ssPM).
+
+Constraints
+
+All input times are valid
+Sample Input 0
+
+07:05:45PM
+Sample Output 0
+
+19:05:45
+Solution:-
+
+*/
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+    public static String timeConversion(String s) {
+   int hour = Integer.parseInt(s.substring(0, 2)) % 12;
+    if (s.endsWith("PM"))
+        hour += 12;
+    return String.format("%02d", hour) + s.substring(2, 8);
+    }  
+    
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String s = bufferedReader.readLine();
+
+        String result = Result.timeConversion(s);
+
+        bufferedWriter.write(result);
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
