b6bf622e17f2d8265765dd5a9b11e782d9c7233b
Shrey Srivastava <72334234+Shrey212001@users.noreply.github.com>
2022-12-28 11:09:43 +0530

    Drawing Book

--- Day87.java
+++ Day87.java
@@ -75,7 +75,8 @@ public static void main(String[] args) {
 Scanner in = new Scanner(System.in);
 int n = in.nextInt();
 int p = in.nextInt();
-int ans = Math.min(p/2,(n-p)/2);
-System.out.println(ans);
+int frontdist = p / 2;
+int backdist = n % 2 == 0 ? (n - p + 1) / 2 : (n - p) / 2;
+System.out.println(Math.min(frontdist, backdist));
 }
 }
